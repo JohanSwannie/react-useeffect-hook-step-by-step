804348a4f6bb3adc9e00a46a2506598e4234b5a0
jswan <jswannieman@gmail.com>
2023-08-19 17:24:15 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"> UseEffect Hook</div>;
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((result) => setItems(result));
+  }, [resourceType]);
+
+  return (
+    <div>
+      <div className="buttons">
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map((item, index) => (
+        <pre key={index}>{JSON.stringify(item)}</pre>
+      ))}
+    </div>
+  );
 }
 
 export default App;
