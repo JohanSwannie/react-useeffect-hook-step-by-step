c0e8c4d9f1a276231ed559dd70921ce2fbc00678
jswan <jswannieman@gmail.com>
2023-08-19 17:47:51 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 function App() {
   const [resourceType, setResourceType] = useState("posts");
   const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
@@ -11,8 +13,24 @@ function App() {
       .then((result) => setItems(result));
   }, [resourceType]);
 
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
+      <div className="window_parameters">
+        <div>Window Width = {windowWidth}</div>
+        <div>Window Height = {windowHeight}</div>
+      </div>
       <div className="buttons">
         <button onClick={() => setResourceType("posts")}>Posts</button>
         <button onClick={() => setResourceType("users")}>Users</button>
